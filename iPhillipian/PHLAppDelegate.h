c591147ed415fbc509d786df28fef90593d7c55a
David Cao <davidcao@Davids-MacBook-Pro.local>
2014-07-16 18:38:36 -0400

    Initial Commit

---
+++ iPhillipian/PHLAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PHLAppDelegate.h
+//  iPhillipian
+//
+//  Created by David Cao on 7/16/14.
+//  Copyright (c) 2014 Phillipian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PHLAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ iPhillipian/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  iPhillipian
+//
+//  Created by David Cao on 7/16/14.
+//  Copyright (c) 2014 Phillipian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "PHLAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([PHLAppDelegate class]));
+    }
+}
